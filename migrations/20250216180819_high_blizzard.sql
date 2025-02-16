7dc5c42ad90b278812bfb01294c24edc0b3d5ca5
Omkar Date <omkardate29@gmail.com>
2025-02-16 23:43:23 +0530

    feat: migration for e_com_products table

---
+++ migrations/20250216180819_high_blizzard.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "e_com_products" (
+	"id" text PRIMARY KEY NOT NULL,
+	"image_url" text NOT NULL,
+	"name" text NOT NULL,
+	"size" text NOT NULL,
+	"color" text NOT NULL,
+	"price" double precision NOT NULL
+);
