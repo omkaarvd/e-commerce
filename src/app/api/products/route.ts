cdf23726b43f5543411dbd30982103b7d61cd511
Omkar Date <omkardate29@gmail.com>
2025-02-21 01:09:01 +0530

    feat: implement product filtering by color, price range, and size with sorting options

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,8 +1,50 @@
 import { db } from "@/db";
 import { productsTable } from "@/db/schema";
+import { ProductFilterValidator } from "@/lib/validators/product-validator";
+import { and, asc, between, desc, inArray, SQL } from "drizzle-orm";
+import { NextRequest } from "next/server";
 
-export const POST = async () => {
-  const products = await db.select().from(productsTable);
+export const POST = async (req: NextRequest) => {
+  try {
+    const body = await req.json();
 
-  return new Response(JSON.stringify(products));
+    const { color, price, size, sort } = ProductFilterValidator.parse(
+      body.filter
+    );
+
+    const filters: SQL[] = [];
+
+    if (color.length) {
+      filters.push(inArray(productsTable.color, color));
+    }
+
+    if (price.length === 2) {
+      filters.push(between(productsTable.price, price[0], price[1]));
+    }
+
+    if (size.length) {
+      filters.push(inArray(productsTable.size, size));
+    }
+
+    const query = db
+      .select()
+      .from(productsTable)
+      .where(and(...filters));
+
+    if (sort === "price-asc") {
+      query.orderBy(asc(productsTable.price));
+    } else if (sort === "price-desc") {
+      query.orderBy(desc(productsTable.price));
+    }
+
+    const products = await query;
+
+    return new Response(JSON.stringify(products));
+  } catch (err) {
+    console.error(err);
+
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -83,7 +83,12 @@ export default function Home() {
     queryKey: ["products"],
     queryFn: async () => {
       const { data } = await axios.post<SelectProduct[]>("/api/products", {
-        filter: { sort: filter.sort },
+        filter: {
+          sort: filter.sort,
+          color: filter.color,
+          price: filter.price.range,
+          size: filter.size,
+        },
       });
 
       return data;
