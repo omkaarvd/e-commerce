a16c118796d57c09fffdc1d4efe1641358e4ed39
Omkar Date <omkardate29@gmail.com>
2025-05-07 15:43:47 +0530

    feat: implement checkout route with Stripe integration and user management

---
+++ src/app/api/products/checkout/route.ts
@@ -0,0 +1,125 @@
+import { db } from "@/db";
+import { productsTable, usersTable } from "@/db/schema";
+import { stripe } from "@/lib/stripe";
+import { auth, currentUser } from "@clerk/nextjs/server";
+import { and, eq, gte } from "drizzle-orm";
+import { NextRequest } from "next/server";
+import Stripe from "stripe";
+import { z } from "zod";
+
+const RequestBodyValidator = z.object({
+  products: z.array(
+    z.object({
+      productId: z.string(),
+      quantity: z.number().min(1),
+    })
+  ),
+});
+
+export async function POST(req: NextRequest) {
+  try {
+    const { userId } = await auth();
+
+    const current_user = await currentUser();
+
+    if (!userId || !current_user) {
+      return new Response(JSON.stringify({ message: "Unauthorized" }), {
+        status: 401,
+      });
+    }
+
+    const body = await req.json();
+    const { products } = RequestBodyValidator.parse(body);
+
+    const productDetails = await Promise.all(
+      products.map(async ({ productId, quantity }) => {
+        const product = await db.query.productsTable.findFirst({
+          where: and(
+            eq(productsTable.id, productId),
+            gte(productsTable.available, quantity)
+          ),
+          columns: {
+            embedding: false,
+            createdAt: false,
+            updatedAt: false,
+          },
+        });
+
+        if (!product) throw new Error("Invalid product");
+
+        return {
+          name: product.name,
+          quantity,
+          price: product.price,
+        };
+      })
+    );
+
+    const lineItems: Stripe.Checkout.SessionCreateParams.LineItem[] =
+      productDetails.map(({ name, price, quantity }) => ({
+        price_data: {
+          currency: "inr",
+          product_data: { name },
+          unit_amount: price * 100,
+        },
+        quantity,
+      }));
+
+    // Get user and Stripe customer ID
+    let user = await db.query.usersTable.findFirst({
+      where: eq(usersTable.id, userId),
+      columns: { email: true, stripeCustomerId: true },
+    });
+
+    if (!user) {
+      user = (
+        await db
+          .insert(usersTable)
+          .values({
+            id: userId,
+            email: current_user.primaryEmailAddress!.emailAddress,
+          })
+          .returning({
+            email: usersTable.email,
+            stripeCustomerId: usersTable.stripeCustomerId,
+          })
+      )[0];
+    }
+
+    let stripeCustomerId = user.stripeCustomerId;
+
+    if (!stripeCustomerId) {
+      const customer = await stripe.customers.create({ email: user.email });
+
+      await db
+        .update(usersTable)
+        .set({ stripeCustomerId: customer.id })
+        .where(eq(usersTable.id, userId));
+
+      stripeCustomerId = customer.id;
+    }
+
+    const session = await stripe.checkout.sessions.create({
+      customer: stripeCustomerId,
+      line_items: lineItems,
+      mode: "payment",
+      billing_address_collection: "required",
+      shipping_address_collection: { allowed_countries: ["IN", "US", "CA"] },
+      success_url: `${process.env.NEXT_PUBLIC_BASE_URL}/thank-you`,
+      cancel_url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      metadata: {
+        userId,
+        products: products
+          .map(({ productId, quantity }) => `${productId}:${quantity}`)
+          .join(","),
+      },
+    });
+
+    return new Response(JSON.stringify({ url: session.url }), { status: 200 });
+  } catch (error) {
+    console.error("Error in checkout route:", error);
+    return new Response(JSON.stringify({ message: "Internal Server Error" }), {
+      status: 500,
+    });
+  }
+}
