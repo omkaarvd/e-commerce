6185889c0c8dbb68e20a411a2d0a3df39e4038a9
Omkar Date <omkardate29@gmail.com>
2025-07-09 13:38:31 +0530

    feat: implement getProductById function for product retrieval

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,35 +1,5 @@
 import ProductDisplay from "@/components/product-display";
-import { SelectProduct } from "@/db/schema";
-import { AXIOS } from "@/lib/axios";
-
-type ProductWithoutMeta = Omit<
-  SelectProduct,
-  "createdAt" | "updatedAt" | "embedding"
->;
-
-const getProduct = async (
-  slug: string
-): Promise<{
-  data: ProductWithoutMeta | null;
-  status: number;
-}> => {
-  try {
-    const { data: product } = await AXIOS.get<ProductWithoutMeta>(
-      `/api/products/${slug}`
-    );
-
-    return {
-      data: product,
-      status: 200,
-    };
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (err) {
-    return {
-      data: null,
-      status: 404,
-    };
-  }
-};
+import { getProductById } from "@/lib/db/products";
 
 export default async function Page({
   params,
@@ -38,9 +8,9 @@ export default async function Page({
 }) {
   const { slug } = await params;
 
-  const result = await getProduct(slug);
+  const product = await getProductById(slug);
 
-  if (!result.data || result.status !== 200) {
+  if (!product) {
     return (
       <h1 className="text-xl font-bold text-center text-red-500 mt-12">
         Product not found.
@@ -48,5 +18,5 @@ export default async function Page({
     );
   }
 
-  return <ProductDisplay product={result.data} />;
+  return <ProductDisplay product={product} />;
 }
---
+++ src/lib/db/products.ts
@@ -0,0 +1,13 @@
+import { db } from "@/db";
+import { productsTable } from "@/db/schema";
+import { eq } from "drizzle-orm";
+
+export const getProductById = async (slug: string) => {
+  const result = await db
+    .select()
+    .from(productsTable)
+    .where(eq(productsTable.id, slug))
+    .limit(1);
+
+  return result[0] || null;
+};
